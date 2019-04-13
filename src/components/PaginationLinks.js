147ad23a6a8d469e4e924787b1d03165c2a52844
hansol <onesorry49@gmail.com>
2019-04-13 22:51:23 +0900

    MODIFY: change to go '/page/1' when first page button is clicked

--- src/components/PaginationLinks.js
+++ src/components/PaginationLinks.js
@@ -2,10 +2,9 @@ import React from 'react'
 import { Pagination } from 'react-bootstrap'
 
 const PaginationLinks =({ currentPage, numberOfPages }) => {
-  // TODO 페이지는 Post에만 둘 예정이므로 1번으로 이동하면 '/'가 아닌 '/page/1'로 이동하도록 변경할 것
   const isFirst = currentPage === 1
   const isLast = currentPage === numberOfPages
-  const previousPage = currentPage - 1 === 1 ? '/' : '/page/' + (currentPage - 1).toString()
+  const previousPage = '/page/' + (currentPage - 1).toString()
   const nextPage = '/page/' + (currentPage + 1).toString()
   return (
     <Pagination className="justify-content-center">
@@ -16,11 +15,11 @@ const PaginationLinks =({ currentPage, numberOfPages }) => {
       )}
 
       {Array.from({ length: numberOfPages }, (_, i) => currentPage === i + 1 ? (
-        <Pagination.Item active key={`page-number${i + 1}`} href={`/${i === 0 ? "" : "page/" + (i + 1)}`}>
+        <Pagination.Item active key={`page-number${i + 1}`} href={`/${"page/" + (i + 1)}`}>
             {i + 1}
         </Pagination.Item>
       ) : (
-        <Pagination.Item key={`page-number${i + 1}`} href={`/${i === 0 ? "" : "page/" + (i + 1)}`}>
+        <Pagination.Item key={`page-number${i + 1}`} href={`/${"page/" + (i + 1)}`}>
             {i + 1}
         </Pagination.Item>
       ))}
