027a6a22962ab6df4d9cecee28f8dadde5fd6e8f
hansol <onesorry49@gmail.com>
2019-04-15 23:46:57 +0900

    INCOMPLETE: solstagram.js styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,34 +1,48 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import Img from 'gatsby-image'
-import Layout from '../components/layout';
-import SEO from '../components/seo';
+import Layout from '../components/layout'
+import SEO from '../components/seo'
 
 import { Container } from 'react-bootstrap'
 
 // TODO 타임라인 스크롤 좀 잘 구현하기 아직도 about page가 부족하다.
-const AboutPage = (props) => {
+const AboutPage = props => {
   return (
     <Layout>
       <SEO title="About" keywords={[`gatsby`, `application`, `react`]} />
       <div className="about">
         {/* 윗 사진 영역 */}
         <div className="banner">
-          <Img className="banner-img" fluid={props.data.imageBanner.childImageSharp.fluid} />
+          <Img
+            className="banner-img"
+            fluid={props.data.imageBanner.childImageSharp.fluid}
+          />
           <h1 className="about-title">About</h1>
         </div>
 
-
         <Container className="mt-5 introduce">
-
           <h1>About</h1>
           <h3>장한솔</h3>
-          <p>아직 학생이므로 관심 가는 분야 닥치는대로 공부해나가는 예비 개발자</p>
-          <h5>특이사항</h5>
-          <p>백준 온라인 저지 700 문제 해결</p>
-          <p>한 달에 독서 2권 이상. 관심 분야: 과학, 사회, 경제경영</p>
-          <p>20살 부터 하루도 빠짐없이 일기 작성 중</p>
+          <p>
+            아직 학생이므로 관심 가는 분야 닥치는대로 공부해나가는 예비 개발자
+          </p>
 
+          <div className="contact">
+            <h5>
+              <i className="fas fa-envelope" />Email
+            </h5>
+            <p>wkdqhrh00@naver.com</p>
+            <h5>
+              <a
+                href="https://github.com/solft"
+                target="_black"
+                rel="noopener noreferrer"
+              >
+                <i className="fab fa-github" />Github
+              </a>
+            </h5>
+          </div>
 
           <section className="timeline">
             <ol>
@@ -41,25 +55,29 @@ const AboutPage = (props) => {
               <li>
                 <div>
                   <time>2018.08</time>
-                  머신러닝, 딥러닝에 흥미가 생겨 파이썬과 scikit-learn, tensorflow 공부
+                  머신러닝, 딥러닝에 흥미가 생겨 파이썬과 scikit-learn,
+                  tensorflow 공부
                 </div>
               </li>
               <li>
                 <div>
                   <time>2018.09</time>
-                  안드로이드 계속 공부 중에 Kotlin이라는 언어를 알게되고 Kotlin으로 안드로이드 개발 시작
+                  안드로이드 계속 공부 중에 Kotlin이라는 언어를 알게되고
+                  Kotlin으로 안드로이드 개발 시작
                 </div>
               </li>
               <li>
                 <div>
                   <time>2018.11</time>
-                  programmers에서 주최한 winter coding 지원. 떨어지고 곧바로 Flutter 공부 시작
+                  programmers에서 주최한 winter coding 지원. 떨어지고 곧바로
+                  Flutter 공부 시작
                 </div>
               </li>
               <li>
                 <div>
                   <time>2018.12</time>
-                  Web 공부 시작 Javascript 부터 차근차근 동시에 Node.js도 같이 공부
+                  Web 공부 시작 Javascript 부터 차근차근 동시에 Node.js도 같이
+                  공부
                 </div>
               </li>
               <li>
@@ -86,10 +104,9 @@ const AboutPage = (props) => {
                   CSS, SASS 등의 디자인과 MobX, Redux 등의 React 심화 공부 중~
                 </div>
               </li>
-              <li></li>
+              <li />
             </ol>
           </section>
-
         </Container>
       </div>
     </Layout>
@@ -107,6 +124,5 @@ export const aboutQuery = graphql`
     }
   }
 `
-
 
 export default AboutPage
--- src/pages/solstagram.js
+++ src/pages/solstagram.js
@@ -1,26 +1,86 @@
 import React from 'react'
-import Layout from '../components/layout';
-import SEO from '../components/seo';
-
-import { Container, Row, Col } from 'react-bootstrap'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
 
 // TODO 솔스타그램 완성 시키기
 const solstagram = () => {
   return (
     <Layout>
       <SEO title="Solstagarm" keywords={[`gatsby`, `application`, `react`]} />
-      <Container>
-        <Row>
-          <Container>
-            
-          </Container>
-        </Row>
-        <Row>
-          <Col>1 of 2</Col>
-          <Col>2 of 2</Col>
-          <Col>2 of 2</Col>
-        </Row>
-      </Container>
+
+      <div className="solstagram-main">
+        <div className="solstagram-profile">
+          <div className="profile-image">
+            <img src="https://via.placeholder.com/150" alt="" />
+          </div>
+
+          <section>
+            <div className="profile-content">
+              <h1 className="profile-name">Hansol</h1>
+              <span className="profile-certificate">
+                <i className="fas fa-certificate" />
+              </span>
+              <button className="profile-follow">팔로우</button>
+              <span>
+                <i className="fas fa-ellipsis-h" />
+              </span>
+            </div>
+            <ul>
+              <li>
+                <span>게시물</span>
+                <span>7</span>
+              </li>
+              <li>
+                <span>팔로워</span>
+                <span>7</span>
+              </li>
+              <li>
+                <span>팔로잉</span>
+                <span>7</span>
+              </li>
+            </ul>
+
+            <div>상태글</div>
+          </section>
+        </div>
+
+        <div className="solstagram-gallery">
+          <div className="gallery-item">
+            <img src="https://via.placeholder.com/250" alt="" />
+            <div className="gallery-item-info">2019.04.15</div>
+          </div>
+
+          <div className="gallery-item">
+            <img src="https://via.placeholder.com/250" alt="" />
+            <div className="gallery-item-info">2019.04.15</div>
+          </div>
+
+          <div className="gallery-item">
+            <img src="https://via.placeholder.com/250" alt="" />
+            <div className="gallery-item-info">2019.04.15</div>
+          </div>
+
+          <div className="gallery-item">
+            <img src="https://via.placeholder.com/250" alt="" />
+            <div className="gallery-item-info">2019.04.15</div>
+          </div>
+
+          <div className="gallery-item">
+            <img src="https://via.placeholder.com/250" alt="" />
+            <div className="gallery-item-info">2019.04.15</div>
+          </div>
+
+          <div className="gallery-item">
+            <img src="https://via.placeholder.com/250" alt="" />
+            <div className="gallery-item-info">2019.04.15</div>
+          </div>
+
+          <div className="gallery-item">
+            <img src="https://via.placeholder.com/250" alt="" />
+            <div className="gallery-item-info">2019.04.15</div>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
