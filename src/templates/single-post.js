579ad433393f5e9a6d1a6c3a7ba2ee9bdac49088
hansol <onesorry49@gmail.com>
2019-04-17 20:05:45 +0900

    ADD: Disqus plugin

--- src/templates/single-post.js
+++ src/templates/single-post.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
 import { Badge } from 'react-bootstrap'
+import { DiscussionEmbed} from 'disqus-react'
 
 import SEO from '../components/seo'
 import Layout from '../components/layout'
@@ -8,15 +9,23 @@ import { slugify } from '../utils/utilityFunctions'
 
 import { Container } from 'react-bootstrap'
 
-const SinglePost = ({ data }) => {
+const SinglePost = ({ data, pageContext }) => {
   const post = data.markdownRemark.frontmatter
+
+  const baseUrl = 'https://splendid-pine.netlify.com/'
+  const disqusShrotname = "https-splendid-pine-netlify-com"
+  const disqusConfig = {
+    identifier: data.markdownRemark.id,
+    title: post.title,
+    url: baseUrl + pageContext.slug
+  }
   return (
     <Layout>
       <SEO title={post.title} />
       <Container className="markdown-body mt-5">
         <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
         <div>
-          <div className="post-tags mr-1">
+          <div className="post-tags mr-1 mt-3 mb-5">
             Tags - 
           </div>
           {post.tags.map(tag => (
@@ -27,6 +36,7 @@ const SinglePost = ({ data }) => {
             </div>
           ))}
         </div>
+        <DiscussionEmbed shortname={disqusShrotname} config={disqusConfig} />
       </Container>
     </Layout>
   )
